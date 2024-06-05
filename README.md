e686f76540c7b4aef05fd52a70d03cc911bc7783
Kriki82543 <47983260+Kriki82543@users.noreply.github.com>
2024-06-05 18:05:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-h7r1go
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Kriki82543/stackblitz-starters-h7r1go)
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+export default function Header() {
+  return (
+    <div>
+      <div class="sidenav">
+        <a href="#default">Home</a>
+        <a href="#news">Services</a>
+        <a href="#clients">Clients</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </div>
+  );
+}
